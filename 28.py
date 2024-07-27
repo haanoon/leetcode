0a67d4b77e98956b284befad420045463878df4c
haanoon <hanoonmavoor@gmail.com>
2024-07-27 23:11:50 +0530

    28 done

---
+++ 28.py
@@ -0,0 +1,7 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        l1,l2=len(haystack),len(needle)
+        for i in range(l1-l2+1):
+            if haystack[i:i+l2]==needle:
+                return i
+        return -1
